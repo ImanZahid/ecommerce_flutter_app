ec56d503823acc3f8b2ecb0a104afb8c0d7379c6
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-04-04 17:05:56 +0300

    adding firebasefirestore

--- lib/firebase/firebase_manager.dart
+++ lib/firebase/firebase_manager.dart
@@ -1,12 +1,18 @@
 import '../firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore;
 
 class FirebaseManager {
+  late final FirebaseFirestore firebaseFirestore;
   FirebaseManager();
 
   Future<void> initializeFirebase() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform
     );
+  }
+
+  Future<void> initalizeFirebaseFirestore() async {
+    firebaseFirestore = FirebaseFirestore.instance;
   }
 }
